ddcabe1ea56f28396c18d64c43a0439031a3d531
Jaiden Popowski <jpopowski19@gmail.com>
2024-04-20 13:39:19 +1000

    gitignore now working

--- copy.R
+++
@@ -1,8 +0,0 @@
-
-#rmarkdown::render_site()
-
-file.copy(
-  from = paste0("C:/Users/jpopo/Documents/Rstats/rWebsite/docs/",list.files(path = "C:/Users/jpopo/Documents/Rstats/rWebsite/docs")),
-  to = "C:/Users/jpopo/Documents/GitHub/jaidenpopowski.github.io",
-  overwrite = T
-  )
